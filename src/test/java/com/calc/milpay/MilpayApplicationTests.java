478cece6b4edb07947dcb2469c785bdf7ce88b2c
mt13p <kozakartem@gmail.com>
2023-03-30 17:17:37 +0300

    Hello world

---
+++ src/test/java/com/calc/milpay/MilpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc.milpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
